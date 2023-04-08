7fa3f3149ab8051030e45fbcaed1008d4625e76b
jacobrees <jacob.rees97@yahoo.com>
2023-04-08 20:25:15 +0100

    Create a basic resizing effect for the jacob rees text

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import './styles.scss';
+import "./styles.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.defaults({
+  markers: true,
+});
+
+window.addEventListener("resize", () => {
+    
+})
+
+const triggerElement = document.querySelector(".home-name-container");
+const targetElement = document.querySelector(".name-title");
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: triggerElement,
+    // trigger element - viewport
+    start: "top center",
+    end: "bottom top",
+    scrub: 1,
+  },
+});
+tl.from(targetElement, {
+  fontSize: 124,
+  duration: 1,
+  y: screen.height / 2,
+});
+
+console.log(targetElement.width, targetElement.height);
